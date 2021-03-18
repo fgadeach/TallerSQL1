64654ed24a6efc225ec4bde4ae438fc60ae3d302
fgadeach <f.gadea@uniandes.edu.co>
2021-03-18 07:31:39 -0500

    Update
    No me habia dado cuenta que habia guardado de forma incorrecta esta parte, así que aproveché para añadir lo del input solo para practicar.

--- EJERCICIO2.sql
+++ EJERCICIO2.sql
@@ -1,3 +1,10 @@
 ALTER SESSION SET CURRENT_SCHEMA = PARRANDEROS; 
-SELECT nombre FROM bebidas
-ORDER BY nombre;
+SELECT aux.nombre, aux.hor, aux.conteo FROM
+(SELECT horario hor, bar.nombre, COUNT(horario) AS Conteo
+FROM Frecuentan f
+Inner Join Bares bar on bar.id = f.id_bar
+Inner Join Bebedores beb on bar.ciudad != beb.ciudad and beb.id=f.id_bebedor
+Inner Join Gustan g on beb.id = g.id_bebedor 
+Inner Join bebidas b on b.id = g.id_bebida and b.nombre = '&jj'
+Group By bar.nombre, horario
+Order By conteo DESC) AUX;
